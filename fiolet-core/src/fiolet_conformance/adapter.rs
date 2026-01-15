33b4a06f33c15ac0219e3bfc4cdfc7878010d1e4
maliszewskiadrian <maliszewskiadrian01@gmail.com>
2026-01-15 10:23:38 +0100

    Update adapter.rs

--- fiolet-core/src/fiolet_conformance/adapter.rs
+++ fiolet-core/src/fiolet_conformance/adapter.rs
@@ -1,6 +1,3 @@
-use fiolet_ageofdarkness::engine::evaluate_trace;
-use fiolet_ageofdarkness::esv::EpistemicTrace;
-
 /// Result of FIOLET conformance validation
 #[derive(Debug, PartialEq)]
 pub enum ConformanceResult {
@@ -19,13 +16,9 @@ pub enum ConformanceResult {
 pub struct FioletConformanceAdapter;
 
 impl FioletConformanceAdapter {
-    pub fn validate(trace: EpistemicTrace) -> ConformanceResult {
-        let result = evaluate_trace(trace);
-
-        if result.is_halt() {
-            ConformanceResult::Halt
-        } else {
-            ConformanceResult::Allow
-        }
+    /// Dummy validate function for testing without AgeOfDarkness
+    pub fn validate<T>(_trace: T) -> ConformanceResult {
+        // Always return Allow by default
+        ConformanceResult::Allow
     }
 }
